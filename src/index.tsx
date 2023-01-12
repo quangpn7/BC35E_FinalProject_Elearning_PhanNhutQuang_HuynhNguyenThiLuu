9757db84381884b076e11513022c56e4dbca4d39
quangpn7 <nhutquang.mca@gmail.com>
2023-01-12 16:56:13 +0700

    setup scss, interceptors (error, asking for help)

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./assets/sass/main.scss";
 import {
   // eslint-disable-next-line
   BrowserRouter,
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,11 @@ import React from "react";
 type Props = {};
 
 const Home = (props: Props) => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <button className="default-btn">Button Ex</button>
+    </div>
+  );
 };
 
 export default Home;
